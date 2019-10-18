c418c765b829f63796ef1fdff599fd55529dbdc3
dams <dams@DESKTOP-533T0GH.lan>
2019-10-18 13:13:24 +0200

    avant checkout

--- fonctions.php
+++ fonctions.php
@@ -8,4 +8,32 @@ add_theme_support( 'title-tag' );
   // for posts
   add_filter('use_block_editor_for_post', '__return_false', 10);
   // for post types
-  add_filter('use_block_editor_for_post_type', '__return_false', 10);
+  add_filter('use_block_editor_for_post');
+
+function capitaine_register_post_types() {
+	
+  // CPT Portfolio
+  $labels = array(
+      'name' => 'Portfolio',
+      'all_items' => 'Tous les projets',  // affiché dans le sous menu
+      'singular_name' => 'Projet',
+      'add_new_item' => 'Ajouter un projet',
+      'edit_item' => 'Modifier le projet',
+      'menu_name' => 'Portfolio'
+  );
+
+$args = array(
+      'labels' => $labels,
+      'public' => true,
+      'show_in_rest' => true,
+      'has_archive' => true,
+      'supports' => array( 'title', 'editor','thumbnail' ),
+      'menu_position' => 5, 
+      'menu_icon' => 'dashicons-admin-customizer',
+);
+
+register_post_type( 'porfolio', $args );
+}
+add_action( 'init', 'capitaine_register_post_types' ); 
+  
+  
--- single.php
+++ single.php
@@ -18,27 +18,8 @@
 
       <div class="post__content">
         <?php the_content(); ?>
-<p>
-    <strong>Avis :</strong> 
-    <?php echo get_post_meta( get_the_ID(), 'avis', true ); ?>
-</p>
-
-<p>
-    <strong>Note :</strong>
-    <?php echo get_post_meta( get_the_ID(), 'note', true ); ?> / 10
-</p>
-
-<div class="plus-moins">
-    <div class="plus">
-        <?php echo get_post_meta( get_the_ID(), 'plus', true ); ?>
-    </div>
-    <div class="plus">
-        <?php echo get_post_meta( get_the_ID(), 'moins', true ); ?>
-    </div>
-</div>
-      </div>
     </article>
-
+    <?php var_dump(the_field('maps')); ?>
   <?php endwhile; endif; ?>
-  <?php get_template_part( 'newsletter' ); ?>
+
 <?php get_footer(); ?>
