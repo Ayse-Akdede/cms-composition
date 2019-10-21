745b4e49fd5eeddbc7df45d718cf8c13a0421d23
Mia Barbir <miabarbir13@gmail.com>
2019-10-21 16:39:23 +0200

    css in progress

--- templates/partsfront/bandeau.php
+++ templates/partsfront/bandeau.php
@@ -1,82 +1,90 @@
 <section class="bandeau">
 			<div class="bandeau-left">
-				<?php if( get_field('bandeau_image_g') ): ?>
-					<img src="<?php the_field('bandeau_image_g'); ?>" />
-				<?php endif; ?>
+				<div class="photo">
+					<?php if( get_field('bandeau_image_g') ): ?>
+						<img src="<?php the_field('bandeau_image_g'); ?>" />
+					<?php endif; ?>
+				</div>
 
-				<h2>
-					<?php 
-						$titre = get_field('bandeau_titre_g', false, false);
-						echo $titre;
-					?>
-				</h2>
-		
+				<div class="left-link-block">
+					<h2>
+						<?php 
+							$titre = get_field('bandeau_titre_g', false, false);
+							echo $titre;
+						?>
+					</h2>
+			
 
-				<?php	
-					$hover = get_field('bandeau_survol_g');	
-					
-					if( $hover ): ?>
-						<div id="hover">
-							<div class="content-hover">
-								<h3><?php echo $hover['titre-g']; ?></h3>
-								<p><?php echo $hover['texte_g']; ?></p>
+					<?php	
+						$hover = get_field('bandeau_survol_g');	
+						
+						if( $hover ): ?>
+							<div id="hover">
+								<div class="content-hover">
+									<h3><?php echo $hover['titre-g']; ?></h3>
+									<p><?php echo $hover['texte_g']; ?></p>
+								</div>
 							</div>
-						</div>
-				<?php endif; ?>
+					<?php endif; ?>
 
 
-				<?php
-					$button = get_field('bandeau_bouton_g');	
-					if( $button ): ?>
-						<div id="button-left">
-							<div class="content-button">
-								<a class="button-left-content" href="<?php echo $button; ?>">
-									<h3><?php echo $button['titre-bouton-g']; ?> &#8594;</h3>
-								</a>
+					<?php
+						$button = get_field('bandeau_bouton_g');	
+						if( $button ): ?>
+							<div id="button-left">
+								<div class="content-button">
+									<a class="button-left-content" href="<?php echo $button; ?>">
+										<p class="link-bandeau"><?php echo $button['titre-bouton-g']; ?> &#8594;</p>
+									</a>
+								</div>
 							</div>
-						</div>
-				<?php endif; ?>
+					<?php endif; ?>
+				</div>
 			</div>
 
 			<!-- RIGHT SIDE -->
 			<div class="bandeau-right">
-				<?php if( get_field('bandeau_image_g') ): ?>
-					<img src="<?php the_field('bandeau_image_d'); ?>" />
-				<?php endif; ?>
+				<div class="photo">
+					<?php if( get_field('bandeau_image_g') ): ?>
+						<img src="<?php the_field('bandeau_image_d'); ?>" />
+					<?php endif; ?>
+				</div>
 
-				<h2>
-					<?php 
-						$titre = get_field('bandeau_titre_d', false, false);
-						echo $titre;
-					?>
-				</h2>
+				<div class="right-link-block">
+					<h2>
+						<?php 
+							$titre = get_field('bandeau_titre_d', false, false);
+							echo $titre;
+						?>
+					</h2>
 
-				<!-- right hover--> 
+					<!-- right hover--> 
 
-				<?php	
-					$hover = get_field('bandeau_survol_d');	
-					
-					if( $hover ): ?>
-						<div id="hover">
-							<div class="content-hover">
-								<h3><?php echo $hover['titre-d']; ?></h3>
-								<p><?php echo $hover['texte_d']; ?></p>
+					<?php	
+						$hover = get_field('bandeau_survol_d');	
+						
+						if( $hover ): ?>
+							<div id="hover">
+								<div class="content-hover">
+									<h3><?php echo $hover['titre-d']; ?></h3>
+									<p><?php echo $hover['texte_d']; ?></p>
+								</div>
 							</div>
-						</div>
-				<?php endif; ?>
+					<?php endif; ?>
 
-				<!-- right button -->
+					<!-- right button -->
 
-				<?php
-					$button = get_field('bandeau_bouton_d');	
-					if( $button ): ?>
-						<div id="button-right">
-							<div class="content-button">
-								<a class="button-right-content" href="<?php echo $button; ?>">
-									<h3><?php echo $button['titre-bouton-d']; ?> &#8594;</h3>
-								</a>
+					<?php
+						$button = get_field('bandeau_bouton_d');	
+						if( $button ): ?>
+							<div id="button-right">
+								<div class="content-button">
+									<a class="button-right-content" href="<?php echo $button; ?>">
+										<p class="link-bandeau"><?php echo $button['titre-bouton-d']; ?> &#8594;</p>
+									</a>
+								</div>
 							</div>
-						</div>
-				<?php endif; ?>
+					<?php endif; ?>
+				</div>
 			</div>
 		</section>
--- templates/partsfront/intro.php
+++ templates/partsfront/intro.php
@@ -1,23 +1,34 @@
 <section class="intro">
+		<div class="intro-left">
+			<div class="block-top">
+				<p>
+					<?php 
+						$text = get_field('intro_texte_intro', false, false);
+						echo $text;
+					?>
+				</p>
+			</div>
 
-			<p>
-				<?php 
-					$text = get_field('intro_texte_intro', false, false);
-					echo $text;
-				?>
-			</p>
+			<div class="block-bottom">
+				<h2>
+					<?php 
+						$title = get_field('intro_titre_intro', false, false);
+						echo $title;
+					?>
+				</h2>
 
-			<h2>
-				<?php 
-					$title = get_field('intro_titre_intro', false, false);
-					echo $title;
-				?>
-			</h2>
+				<p><?php the_field('intro_wysiwyg_intro'); ?></p>
 
-			<p><?php the_field('intro_wysiwyg_intro'); ?></p>
-
-			<a class="link_intro" href="<?php the_field('intro_lien_intro')  ?>">
-				<p>Le magasin</p>
-			</a>
-			
+				<a class="link_intro" href="<?php the_field('intro_lien_intro')  ?>">
+					<p>Le magasin</p>
+				</a>
+			</div>
+		</div>
+		<div class="intro-right">
+			<div class="photo">
+				<?php if( get_field('intro_image_intro') ): ?>
+					<img src="<?php the_field('intro_image_intro'); ?>" />
+				<?php endif; ?>
+			</div>
+		</div>
 	</section>
