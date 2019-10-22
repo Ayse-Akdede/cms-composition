d051bf773b78a7f132ba568ace9d38342e257d27
Ayse Akdede <ayseazize.akdede@gmail.com>
2019-10-22 16:58:18 +0200

    ajout des modifs

--- archive.php
+++ archive.php
@@ -1,40 +1,46 @@
 <?php get_header(); ?>
 
  	<h1>Architecte d'intérieur en action</h1>
-	
-	<?php 
-		$query = new WP_Query(array('post_type' => 'news',));
-			
-		if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post(); ?>
-	
+	 <div class="content">
+		<?php 
+			$query = new WP_Query(array('post_type' => 'news',));
+				
+			if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post(); ?>
+
 		<article class="post">
-		
-		
-	<?php	while ( have_rows('flexible_content') ) : the_row();
+			
+			
+			<?php	while ( have_rows('flexible_content') ) : the_row();
+				if( get_row_layout() == 'titre' ):
+			?>
 
-if( get_row_layout() == 'titre' ):
-  ?>
+			<a href="<?php the_permalink(); ?>" class="post__link">
+				<h2><?php the_sub_field('titre_bloc');?></h2>
+			</a>
 
-	<a href="<?php the_permalink(); ?>" class="post__link">
-		<h2><?php the_sub_field('titre_bloc');?></h2>
-	</a>
+			<?php
+				elseif( get_row_layout() == 'content_image'):    
+			?>
 
-  <?php
+			<div class="image-container">
+				<div class="image" style="background-image: url(<?php echo get_sub_field('img_bloc')['url']; ?>)"/>
+				</div>
+			</div>
 
-elseif( get_row_layout() == 'content_image'):    
-  ?>
-<img src="<?php echo get_sub_field('img_bloc')['url']?>" alt="" width="20%">
-<?php
-endif;
+			<?php
+				endif;
 
-endwhile;
-
-		?>
-            <p class="post__meta">
-                Publié le <?php the_time( get_option( 'date_format' ) ); ?> 
-            </p>	
-			  
+				endwhile;
+			?>
+			
+			<p class="post__meta">
+				Publié le <?php the_time( get_option( 'date_format' ) ); ?> 
+			</p>	
+				
 		</article>
+	
+	
 
-	<?php endwhile; endif; ?>
+		<?php endwhile; endif; ?>
+	</div>
 <?php get_footer(); ?>
